9b4567ae5037a84d3e08eb73689c042bf1cc8806
佛系阿宅 <3374918255@qq.com>
2024-12-15 14:43:08 +0800

    refactor:修改文件结构

--- server/common/image.go
+++ server/common/image.go
@@ -1,7 +1,6 @@
 package common
 
 import (
-	"fmt"
 	"github.com/FXAZfung/random-image/internal/config"
 	"github.com/FXAZfung/random-image/internal/logger"
 	"github.com/FXAZfung/random-image/pkg/utils"
@@ -12,9 +11,5 @@ func InitImages() {
 	MapImages, err = utils.GetImagesFromSubDir(config.MainConfig.File.Path)
 	if err != nil {
 		logger.Logger.Fatalf("Error loading images: %v", err)
-	}
-	//展示图片分类
-	for k, v := range MapImages {
-		fmt.Printf("Category: %s, Images: %v", k, v)
 	}
 }
